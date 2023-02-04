ab095e1bda2b9824ac94b5cb381ebdb6ecfd41c6
Prominendt <hcebrink@gmail.com>
2023-02-04 14:39:04 +0100

    test pl

--- scripts/deploy_template_to_iotedge.sh
+++ scripts/deploy_template_to_iotedge.sh
@@ -7,10 +7,6 @@ DEPLOYMENT_VERSION=$(date +%Y%m%d%H%M%S)
 TARGET_IOT_HUB=$1
 TARGET_CONDITION=$2
 
-# Make sure the following environment variables are set:
-# AZ_IOTHUB_NAME
-# AZ_IOTHUB_RESOURCE_GROUP
-
 az config set extension.use_dynamic_install=yes_without_prompt
 
 az iot edge deployment create \
@@ -18,4 +14,6 @@ az iot edge deployment create \
     -n ${TARGET_IOT_HUB} \
     --content "/tmp/deployment.tmp.json" \
     --target-condition ${TARGET_CONDITION} \
-    --priority 100
+    --priority 100 \
+    --verbose \
+    --debug
